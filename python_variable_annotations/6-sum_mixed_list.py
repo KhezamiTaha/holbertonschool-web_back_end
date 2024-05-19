93bdee952cca290dc62986a8d9b48a58ce45bd34
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-19 09:13:45 +0100

    Using Union[int, float]

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' 6-sum_mixed_list.py vaiable annotations '''
+
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[int, str]]) -> float:
+    """sum the list of floats"""
+    return sum(input_list)
