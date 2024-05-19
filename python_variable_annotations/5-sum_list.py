9de86819034d1772bfb9b841db0995396d9f2279
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-19 09:08:20 +0100

    defUsing List from typing Module

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' 5-sum_list.py vaiable annotations '''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum the list of floats"""
+    return sum(input_list)
