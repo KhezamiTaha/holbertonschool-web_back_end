43dc1cd1108c24081551c19d53b16362bef346d5
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-23 17:38:49 +0100

    implementing time.perf_counter and asyncio.run()

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """calculate average time"""
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
