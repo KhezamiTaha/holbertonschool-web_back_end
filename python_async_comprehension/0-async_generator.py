df3e5a23233589871d41237b3266038d5935d397
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-24 09:41:17 +0100

    using yield and generators

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator()-> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """using yield and generators"""
     for _ in range(10):
         await asyncio.sleep(1)
