be3753578f81d64969e6226a924e196c6c583d7d
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-24 16:22:01 +0100

    using asyncio.gather()

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,7 @@
 
 import asyncio
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' 2-measure_runtime.py python_async_comprehension '''
+
+
+import asyncio
+import time
+from typing import List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """"using asyncio.gather()"""
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+    return end_time - start_time
