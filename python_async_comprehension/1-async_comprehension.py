1894099961d62b65786f46f3ef7958f4148f42cd
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-24 09:50:15 +0100

    using async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' 1-async_comprehension.py python_async_comprehension '''
+
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """using async comprehension"""
+    return [num async for num in async_generator()]
