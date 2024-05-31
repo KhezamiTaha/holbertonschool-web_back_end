5893c0ae3c910d5a53cf376e9e1adbaad01c3750
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-31 10:22:01 +0100

    Deletion-resilient hypermedia pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -16,7 +16,7 @@ def index_range(page: int, page_size: int) -> tuple:
         page_size (int): The number of items per page.
 
     Returns:
-        tuple: A tuple containing the start index and end index.
+        tuple: A tuple containing the  start index and end index.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
