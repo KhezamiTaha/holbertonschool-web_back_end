627d5ee3afdb672fbdc988de2e85d9a673aae1c8
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-31 10:04:06 +0100

    Get a page from the datas

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-''' 1-simple_pagination.py pagination '''
+''' 2-hypermedia_pagination.py pagination '''
 
 
 import csv
 import math
-from typing import List
+from typing import List, Dict, Any
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """
     Calculate the start and end indexes for pagination.
     """
@@ -48,3 +48,19 @@ class Server:
             return []
 
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Get a page from the dataset and additional pagination details.
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
