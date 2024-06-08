f164d655e6e8d813e5f9217c50c0a1cf1ba6a504
KhezamiTaha <khezamitaha10@gmail.com>
2024-06-08 11:32:30 +0100

    eslint

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,37 +1,36 @@
-
 module.exports = {
-    env: {
+  env: {
       browser: false,
       es6: true,
       jest: true,
-    },
-    extends: [
+  },
+  extends: [
       'airbnb-base',
       'plugin:jest/all',
-    ],
-    globals: {
+  ],
+  globals: {
       Atomics: 'readonly',
       SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
+  },
+  parserOptions: {
       ecmaVersion: 2018,
       sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
+  },
+  plugins: ['jest'],
+  rules: {
       'no-console': 'off',
       'no-shadow': 'off',
       'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
+          'error',
+          'LabeledStatement',
+          'WithStatement',
       ],
-    },
-    overrides:[
+      'indent': ['error', 4],
+  },
+  overrides: [
       {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
+          files: ['*.js'],
+          excludedFiles: 'babel.config.js',
       }
-    ]
-  };
-  
+  ]
+};
