151920e87a71c3e9b0c5e43f547cc5e8382828c3
KhezamiTaha <khezamitaha10@gmail.com>
2024-06-10 11:56:51 +0100

    implementing a course class and a getter and a setter for each attribute

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,61 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof (length) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Expected an array for students');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Expected an array of strings for students');
+    }
+    this._name = name;
+    this._lenght = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Expected an array for students');
+    }
+    if (!newStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Expected an array of strings for students');
+    }
+    this._students = newStudents;
+  }
+}
