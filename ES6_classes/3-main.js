189ad74d2b7dc8b6765d86ba67f5510ee7fdaf04
KhezamiTaha <khezamitaha10@gmail.com>
2024-06-11 10:17:20 +0100

    implementing a Pricing class using instanceof

--- ES6_classes/3-main.js
+++ ES6_classes/3-main.js
@@ -1,4 +1,7 @@
 import Currency from "./3-currency.js";
 
 const dollar = new Currency('$', 'Dollars');
+console.log(typeof dollar);
+console.log(typeof 'ssss');
+console.log(typeof 3);
 console.log(dollar.displayFullCurrency());
---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof (amount) !== 'number') {
+      throw new TypeError('amount must be a Number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for name
+  set amount(newamount) {
+    if (typeof (newamount) !== 'number') {
+      throw new TypeError('amount must be a Number');
+    }
+    this._amount = newamount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for name
+  set currency(newcurrency) {
+    if (newcurrency instanceof Currency) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = newcurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
