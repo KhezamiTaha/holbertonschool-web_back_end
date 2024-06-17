ea369e859bd286acacbf24c7c85c03e3757208e7
KhezamiTaha <khezamitaha10@gmail.com>
2024-06-17 18:33:17 +0100

    implementing a class using Symbol()

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,3 +1,16 @@
-export default class Car{
-  
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
 }
