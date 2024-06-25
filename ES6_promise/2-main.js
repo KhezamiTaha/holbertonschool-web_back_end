7d4fc18a6291bee9fa47b4cdf78b739395c20806
KhezamiTaha <khezamitaha10@gmail.com>
2024-06-25 18:36:46 +0100

    setup

--- ES6_promise/2-main.js
+++ ES6_promise/2-main.js
@@ -1,4 +1,7 @@
 import handleResponseFromAPI from "./2-then";
 
 const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+handleResponseFromAPI(promise).then((result) => {
+  console.log(result); // Logs: { status: 200, body: 'success' }
+});
+
