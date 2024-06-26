80bdf60604c653af7e01208b92a2933e51cde5ae
KhezamiTaha <khezamitaha10@gmail.com>
2024-06-26 08:52:10 +0100

    resolving multiple promises using Promise.all([])

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([object1, object2]) => {
       console.log(object1.body, object2.firstName, object2.lastName);
     }).catch(() => {
